428ccb0df9823c3e7e1afb39d8eb1bd3f8d3a78f
Ashutosh Kr Singh <ashutoshkrsinghsm@gmail.com>
2024-12-23 10:38:08 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Let&apos;s host a Next.js app on Azure <br />
-      Is it changed?
+      Is it changed? No
     </main>
   );
 }
